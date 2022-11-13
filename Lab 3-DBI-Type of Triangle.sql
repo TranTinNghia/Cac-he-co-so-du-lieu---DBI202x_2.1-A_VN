8f417b4e1ff515e487440a6d3bc0f00bd98ae722
Tran Tin Nghia <114141200+TranTinNghia@users.noreply.github.com>
2022-11-13 23:19:41 +0700

    Add files via upload

---
+++ Lab 3-DBI-Type of Triangle.sql
@@ -0,0 +1,7 @@
+﻿SELECT  CASE
+        WHEN A+B<=C OR A+C<=B OR B+C<=A THEN 'Not A Triangle'
+        WHEN A = B AND B = C then 'Equilateral'
+        WHEN A = B OR B = C OR A = C then 'Isosceles'
+        ELSE 'Scalene'
+        END
+FROM    TRIANGLES;
