f57150bdbbd74e12d1d6aa2a28b8b27cf7dc9ddb
Tran Tin Nghia <114141200+TranTinNghia@users.noreply.github.com>
2022-11-15 15:34:09 +0700

    Add files via upload

---
+++ Lab 4-DBI-Binary Tree Nodes.sql
@@ -0,0 +1,8 @@
+﻿SELECT  N,
+        (CASE   WHEN P IS NULL THEN 'Root'
+                WHEN (SELECT COUNT(*) FROM BST WHERE P = B.N) > 0 THEN 'Inner'
+                ELSE 'Leaf'
+                END
+        ) AS TYPE_OF_BT
+FROM BST AS B
+ORDER BY N
---
+++ Lab 4-DBI-Weather Observation Station 5.sql
@@ -0,0 +1,20 @@
+﻿WITH TEMP1 AS (
+        SELECT CITY, LEN(CITY) AS LEN_CITY
+        FROM STATION
+),   TEMP2 AS (
+        SELECT DENSE_RANK() OVER(PARTITION BY LEN_CITY ORDER BY CITY) AS DENSE_RANK, *
+        FROM TEMP1
+),   TEMP3 AS (
+        SELECT  *,
+                (CASE   WHEN    (DENSE_RANK = (SELECT MIN(DENSE_RANK) FROM TEMP2)
+                                AND LEN_CITY = (SELECT MIN(LEN_CITY) FROM TEMP1)) THEN 'MIN'
+                        WHEN    (DENSE_RANK = (SELECT MIN(DENSE_RANK) FROM TEMP2)
+                                AND LEN_CITY = (SELECT MAX(LEN_CITY) FROM TEMP1)) THEN 'MAX'
+                        ELSE    NULL
+                        END
+                ) AS FLAG
+        FROM TEMP2
+)
+SELECT CITY, LEN_CITY
+FROM TEMP3
+WHERE FLAG IS NOT NULL
